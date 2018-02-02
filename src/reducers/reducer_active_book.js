6e99ca0a0f3bca33c0726ea5c458336a08b19e21
nigel-riss <mr.kurenkov@gmail.com>
2018-02-02 11:44:08 +0200

    break

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+// state agrument isn not an app state, only the state this reducer is responsible for
+export default function(state = null, action) {
+    switch (action.type) {
+        case "BOOK_SELECTED":
+            return action.payload;
+        default:
+            return state;
+    }
+}
